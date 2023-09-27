a9175fa5fd56dbda453efc8868f7c711a4e87ec4
taichi-km <torinos947@gmail.com>
2023-09-27 14:51:45 +0900

    JSでレスポンスを処理する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,3 +1,19 @@
+const buildHTML = (XHR) => {
+  const item = XHR.response.post;
+  // レスポンスの中から投稿されたメモの情報を抽出し、変数itemに格納する
+  // postsコントローラーのcreateアクションでpostというキーと投稿されたメモの内容が紐付いている
+  const html = `
+    <div class="post">
+      <div class="post-date">
+        投稿日時：${item.created_at}
+      </div>
+      <div class="post-content">
+        ${item.content}
+      </div>
+    </div>`;
+  return html;
+};
+
 function post (){
   const form = document.getElementById("form");
   form.addEventListener("submit", (e) => {
@@ -8,11 +24,27 @@ function post (){
     // フォームに入力された値をformDataに格納する
     const XHR = new XMLHttpRequest();
     // JavaScriptがサーバーサイドにリクエストを送信するのに必要なXMLHttpRequestオブジェクトを生成
-    XHR.open("POST", "/posts", true)
+    XHR.open("POST", "/posts", true);
     // openメソッドを用いてHTTPメソッドの指定、パスの指定、非同期通信のON/OFFを指定する
     XHR.responseType = "json";
     // レスポンスのデータフォーマットをjsonに指定する
     XHR.send(formData);
+    XHR.onload = () => {
+    // リクエストの送信が成功したときに処理が実行される
+      if (XHR.status != 200) {
+      // HTTPステータスコードが200以外（リクエストが失敗）の場合
+        alert(`Error ${XHR.status}: ${XHR.statusText}`);
+        return null;
+        // JavaScriptの処理から抜け出し、これ以降の処理を行わないようにする
+      };
+      const list = document.getElementById("list");
+      const formText = document.getElementById("content");
+      // item内に格納されたメモの情報を元に、ブラウザに描画するためのHTMLを生成し、変数htmlに格納
+      list.insertAdjacentHTML("afterend", buildHTML(XHR));
+      // 変数listに格納された要素の直後に生成したHTMLを挿入する
+      formText.value = "";
+      // formTextのvalue属性に空の文字列を指定することで、フォームの中身をリセットする
+    };
   });
 };
   
